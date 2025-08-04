1f717462836df934dbc061df9149995b5f058546
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-08-04 23:59:13 +0600

    global error handler customized

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -5,6 +5,49 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-google-oauth20";
 import { User } from "../modules/user/user.model";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs"
+
+passport.use(
+    new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password"
+    }, async (email: string, password: string, done) => {
+        try {
+            const isUserExist = await User.findOne({ email })
+
+            // if (!isUserExist) {
+            //     return done(null, false, { message: "User does not exist" })
+            // }
+
+            if (!isUserExist) {
+                return done("User does not exist")
+            }
+
+            const isGoogleAuthenticated = isUserExist.auths.some(providerObjects => providerObjects.provider == "google")
+
+            if (isGoogleAuthenticated && !isUserExist.password) {
+                return done(null, false, { message: "You have authenticated through Google. So if you want to login with credentials, then at first login with google and set a password for your Gmail and then you can login with email and password." })
+            }
+
+            // if (isGoogleAuthenticated  && !isUserExist.password) {
+            //     return done("You have authenticated through Google. So if you want to login with credentials, then at first login with google and set a password for your Gmail and then you can login with email and password.")
+            // }
+
+            const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
+
+            if (!isPasswordMatched) {
+                return done(null, false, { message: "Password does not match" })
+            }
+
+            return done(null, isUserExist)
+
+        } catch (error) {
+            console.log(error);
+            done(error)
+        }
+    })
+)
 
 passport.use(
     new GoogleStrategy(
